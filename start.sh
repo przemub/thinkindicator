af30ab07270b9372406a247d4cad91da7642d4a6
Przemysław Buczkowski <prem@prem.moe>
2021-02-02 19:17:32 +0100

    Python packaging

--- start.sh
+++ start.sh
@@ -1,5 +1,1 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export PYTHONPATH="$DIR:$PYTHONPATH"
-
-python -m thinkindicator
+scripts/thinkindicator
